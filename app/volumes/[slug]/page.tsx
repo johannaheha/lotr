53bf1f9c990758dbe38d257ad79d6c6ca1af8cd6
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-11-15 16:29:37 +0100

    add previous and next button

--- app/volumes/[slug]/page.tsx
+++ app/volumes/[slug]/page.tsx
@@ -7,28 +7,58 @@ export default async function Page({
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
+  const volume = volumes.find((volume) => volume.slug === slug);
+  const index = volumes.findIndex((volume) => volume.slug === slug);
 
+  if (!volume) {
+    return (
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+          <Link href="/volumes">Back to Volumes</Link>
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Volume Not Found
+          </h1>
+        </main>
+      </div>
+    );
+  }
   return (
-    console.log(volumes[0].slug),
+    console.log(volumes[2].slug),
     (
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
           <Link href="/volumes">Back to Volumes</Link>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            {volumes.find((volume) => volume.slug === slug)?.title ||
-              "1Volume Not Found"}
+            {volume.title}
           </h1>
-          <p>
-            {volumes.find((volume) => volume.slug === slug)?.description ||
-              "Description Not Found"}
-          </p>
+          <p>{volume.description}</p>
           <ul>
-            {volumes
-              .find((volume) => volume.slug === slug)
-              ?.books.map((book) => (
-                <li key={book.ordinal}>{book.title}</li>
-              ))}
+            {volume.books.map((book, index) => (
+              <li key={index}>
+                {book.ordinal};{book.title}
+              </li>
+            ))}
           </ul>
+          <p>
+            {index === 0 && (
+              <Link href={"/volumes/" + volumes[index + 1].slug}>Next</Link>
+            )}
+          </p>
+          <p>
+            {index > 0 && index < volumes.length - 1 && (
+              <>
+                <Link href={"/volumes/" + volumes[index - 1].slug}>
+                  Previous
+                </Link>
+                <Link href={"/volumes/" + volumes[index + 1].slug}>Next</Link>
+              </>
+            )}
+          </p>
+          <p>
+            {index === volumes.length - 1 && (
+              <Link href={"/volumes/" + volumes[index - 1].slug}>Previous</Link>
+            )}
+          </p>
         </main>
       </div>
     )
--- app/volumes/page.tsx
+++ app/volumes/page.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { introduction } from "../../resources/lib/data";
 import { volumes } from "../../resources/lib/data";
 
-
 export default function PageVolume() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -12,12 +11,25 @@ export default function PageVolume() {
         </h1>
         <p>{introduction}</p>
         <h2>All Volumes</h2>
-        <ul><li>{volumes[0].title}
-            <Link href={"the-fellowship-of-the-ring"}>The fellowship of the ring</Link></li></ul>
-        <ul><li>{volumes[1].title}
-            <Link href={"the-two-towers"}>The two towers</Link></li></ul>
-        <ul><li>{volumes[2].title}
-            <Link href={"/the-return-of-the-king"}>The return of the king</Link></li></ul>
+        <ul>
+          <li>
+            <Link href={"volumes/the-fellowship-of-the-ring"}>
+              {volumes[0].title}
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link href={"volumes/the-two-towers"}>{volumes[1].title}</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link href={"volumes/the-return-of-the-king"}>
+              {volumes[2].title}
+            </Link>
+          </li>
+        </ul>
       </main>
     </div>
   );
